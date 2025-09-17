6a21c2383a6021cc302b210c549e38461be6066d
Rudex Tede <rudexjuniortede@gmail.com>
2025-09-17 23:18:21 +0100

    Complete exercises

--- src/page/Survey.tsx
+++ src/page/Survey.tsx
@@ -1,11 +1,19 @@
-import { useParams } from "react-router"
+import { Link, useParams } from "react-router"
 
 export default function Survey() {
     const { questionNumber } = useParams()
+    const currentNumber = questionNumber ? parseInt(questionNumber, 10) : 1
+    const prev = currentNumber === 1 ? currentNumber : currentNumber - 1
+    const next = currentNumber + 1
     return (
         <div>
             <h1>Questionnaire 🧮</h1>
             <h2>Question {questionNumber}</h2>
+
+            <nav>
+                <Link to={`/survey/${prev}`}>Précédent</Link>
+                <Link to={currentNumber !== 10 ? `/survey/${next}` : `/results`}>Suivant</Link>
+            </nav>
         </div>
     )
 }
