1085c2628ceb21bed72da3cb8dd9939a425f48c2
Rudex Tede <rudexjuniortede@gmail.com>
2025-09-17 23:41:35 +0100

    Add freelance card component

--- src/page/Freelances.tsx
+++ src/page/Freelances.tsx
@@ -1,7 +1,38 @@
+import DefaultPicture from '../assets/profile.png'
+import Card from '../components/Card'
+
+const freelanceProfiles = [
+    {
+        name: 'Jane Doe',
+        jobTitle: 'Devops',
+        picture: DefaultPicture,
+    },
+    {
+        name: 'John Doe',
+        jobTitle: 'Developpeur frontend',
+        picture: DefaultPicture,
+    },
+    {
+        name: 'Jeanne Biche',
+        jobTitle: 'Développeuse Fullstack',
+        picture: DefaultPicture,
+    },
+]
+
 function Freelances() {
     return (
         <div>
-            <h1>Freelance</h1>
+            <h1>Freelances 👩·💻👨·💻👩·💻</h1>
+            {
+                freelanceProfiles.map((profile, index) => (
+                    <Card
+                        key={`${profile.name}-${index}`}
+                        label={profile.jobTitle}
+                        picture={profile.picture}
+                        title={profile.name}
+                    />
+                ))
+            }
         </div>
     )
 }
